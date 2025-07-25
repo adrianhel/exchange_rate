9652d32ab7683019915c2e60aa130b091178e298
andy <has32photo@gmail.com>
2025-07-25 09:54:39 +0300

    Update file

--- data/scripts/from_api_to_ch_script_xg.py
+++ data/scripts/from_api_to_ch_script_xg.py
@@ -2,17 +2,10 @@ import requests as req                   # для выполнения HTTP-за
 import pandas as pd                      # для обработки данных
 import csv                               # для работы с CSV
 from clickhouse_driver import Client     # для подключения к ClickHouse
-import os
-from dotenv import load_dotenv
 
-# Загружаем переменные окружения из файла .env
-load_dotenv()
-
-# Получаем токен доступа из переменной окружения
-api_token = os.getenv('API_TOKEN')
 
 # Наш линк с токеном и датой
-URL = f'https://api.exchangerate.host/timeframe?access_key={api_token}&source=USD&start_date=2025-01-01&end_date=2025-01-01'
+URL = f'https://api.exchangerate.host/timeframe?access_key=043dc9dad696914726d3064e9d917294&source=USD&start_date=2025-01-01&end_date=2025-01-01'
 
 # Функция для извлечения данных с API
 def extract_data(url, csv_file):
@@ -20,6 +13,9 @@ def extract_data(url, csv_file):
     if response.status_code != 200:
         print(f"Ошибка API: {response.status_code}, {response.text}")
         return
+    else:
+        data = response.json()
+        print(data)  # Выводим ответ для проверки
 
     data = response.json()
     # Запись данных в CSV-файл

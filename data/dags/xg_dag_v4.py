e5c0ad82f149d5f7e291251a159af89bd75ffddc
andy <has32photo@gmail.com>
2025-09-27 10:30:35 +0300

    Update file

--- data/dags/xg_dag_v4.py
+++ data/dags/xg_dag_v4.py
@@ -1,19 +1,19 @@
-import requests as req                               # для выполнения HTTP-запросов
-import pandas as pd                                  # для обработки данных
-import csv                                           # для работы с CSV
-from clickhouse_driver import Client                 # для подключения к ClickHouse
-from airflow import DAG                              # объект DAG, ключевой элемент Airflow
+import requests as req  # для выполнения HTTP-запросов
+import pandas as pd  # для обработки данных
+import csv  # для работы с CSV
+from clickhouse_driver import Client  # для подключения к ClickHouse
+from airflow import DAG  # объект DAG, ключевой элемент Airflow
 from airflow.operators.python import PythonOperator  # с помощью которого него будем запускать Python код
-from airflow.utils.dates import days_ago             # модуль, связанный с обработкой дат
-from datetime import datetime                        # для даты
-from airflow.hooks.base_hook import BaseHook         # для хуков
-from airflow.models import Variable                  # для глобальных переменных
+from airflow.utils.dates import days_ago  # модуль, связанный с обработкой дат
+from datetime import datetime  # для даты
+from airflow.hooks.base_hook import BaseHook  # для хуков
+from airflow.models import Variable  # для глобальных переменных
 from airflow_clickhouse_plugin.operators.clickhouse import ClickHouseOperator
+from airflow.providers.telegram.operators.telegram import TelegramOperator
 
-
-NAME = "andy_xg_v4"               # Имя для DAG и таблицы в ClickHouse
-TOKEN = Variable.get('TOKEN')     # Токен для API тянем из Variables
-CURRENCY = 'USD'                  # Валюта, которая нас интересует
+NAME = "andy_xg_notify"  # Имя для DAG и таблицы в ClickHouse
+TOKEN = Variable.get('TOKEN')  # Токен для API тянем из Variables
+CURRENCY = 'USD'  # Валюта, которая нас интересует
 URL = (f'https://api.exchangerate.host/timeframe?access_key={TOKEN}'
        f'&source={CURRENCY}&start_date={{{{ ds }}}}&end_date={{{{ ds }}}}')  # Составной URL нашего API
 
@@ -25,11 +25,12 @@ PASSWORD = BaseHook.get_connection("clickhouse_default").password
 DATABASE = BaseHook.get_connection("clickhouse_default").schema
 
 CH_CLIENT = Client(
-    host=HOST,          # IP-адрес сервера ClickHouse
-    user=USER,          # Имя пользователя для подключения
+    host=HOST,  # IP-адрес сервера ClickHouse
+    user=USER,  # Имя пользователя для подключения
     password=PASSWORD,  # Пароль для подключения
-    database=DATABASE   # База данных, к которой подключаемся
+    database=DATABASE  # База данных, к которой подключаемся
 )
+
 
 # Функция для извлечения данных с API
 def extract_data(url, csv_file):
@@ -59,35 +60,63 @@ def upload_to_clickhouse(csv_file, table_name, client):
     # Чтение данных из CSV
     data_frame = pd.read_csv(csv_file)
     # Преобразование в float
-    data_frame['value'] = data_frame['value'].astype(float)     # Преобразование в float
+    data_frame['value'] = data_frame['value'].astype(float)  # Преобразование в float
 
     # Запись data frame в ClickHouse
     client.execute(f'INSERT INTO {table_name} VALUES', [tuple(x) for x in data_frame.to_numpy()])
 
-# Определяем DAG, это контейнер для описания нашего пайплайна
+
+# Функция для обработки ошибки и отправки сообщения
+def task_failure_callback(context):
+    send_message = TelegramOperator(
+        task_id='send_message_telegram',
+        telegram_conn_id='telegram_andy',
+        chat_id='-1003046951828',
+        text='❌ Задача «' + context.get('task_instance').task_id + '» не выполнена.',
+        dag=dag)
+    return send_message.execute(context=context)
+
+
+# Функция для обработки ошибки и отправки сообщения
+def task_success_callback(context):
+    send_message = TelegramOperator(
+        task_id='send_message_telegram',
+        telegram_conn_id='telegram_andy',
+        chat_id='-1003046951828',
+        text='✅ Задача «' + context.get('task_instance').task_id + '» выполнена.',
+        dag=dag)
+    return send_message.execute(context=context)
+
+
+# Определение DAG
+default_args = {
+    'on_failure_callback': task_failure_callback,  # Задача не выполнена
+    'on_success_callback': task_success_callback,  # Задача выполнена
+}
+
 dag = DAG(
     dag_id=NAME,
-    schedule_interval='@daily',         # Как часто запускать, счит. CRON запись
-    start_date=datetime(2025,9,21),     # Начало загрузки
-    end_date=datetime(2025,9,25),       # Конец загрузки
-    max_active_runs=1,                  # Будет запускать только 1 DAG за раз
-    tags=["andy", "xg"]                 # Тэги на свое усмотрение
+    default_args=default_args,
+    schedule_interval='@daily',  # Как часто запускать, счит. CRON запись
+    start_date=datetime(2025, 9, 21),  # Начало загрузки
+    end_date=datetime(2025, 9, 25),  # Конец загрузки
+    max_active_runs=1,  # Будет запускать только 1 DAG за раз
+    tags=["andy", "xg"]  # Тэги на свое усмотрение
 )
 
 # Задача для извлечения данных
 task_extract = PythonOperator(
-    task_id='extract_data',            # Уникальное имя задачи
-    python_callable=extract_data,      # Функция, которая будет запущена (определена выше)
-
+    task_id='extract_data',  # Уникальное имя задачи
+    python_callable=extract_data,  # Функция, которая будет запущена (определена выше)
     # Параметры в виде списка которые будут переданы в функцию "extract_data"
     op_args=[URL, './extracted_data.csv'],
-    dag=dag,    # DAG к которому приклеплена задача
+    dag=dag,  # DAG к которому приклеплена задача
 )
 
 # Оператор для выполнения запроса
 create_table = ClickHouseOperator(
     task_id='create_table',
-    sql='CREATE TABLE IF NOT EXISTS andy_xg_v4 (start_date String, source String, char_code String, value Float64) ENGINE Log',
+    sql='CREATE TABLE IF NOT EXISTS andy_xg_notify (start_date String, source String, char_code String, value Float64) ENGINE Log',
     clickhouse_conn_id='clickhouse_default',
     dag=dag,
 )
